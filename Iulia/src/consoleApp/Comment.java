e699dd4752ce9d2082b0b2613a9cd958c2527410
a-violeta <iulia.violeta.avram@gmail.com>
2026-07-03 22:18:07 +0300

    Add Comment class

---
+++ Iulia/src/consoleApp/Comment.java
@@ -0,0 +1,56 @@
+package consoleApp;
+
+public class Comment {
+
+    private static long idIncrementor = 0;
+    // for id uniqueness, ids given will be 1, then 2, 3 ...
+
+    private long commentId;
+    private String text;
+    private long userId;
+    private long postId;
+    // missing date for now
+
+    // validations not made here
+
+    private static long incrementId(){
+        idIncrementor++;
+        return idIncrementor;
+    }
+
+    Comment(){
+        this.commentId = incrementId();
+        this.text="";
+        this.userId=0;
+        this.postId=0;
+    }
+
+    Comment(String text, long userId, long postId){
+        this.commentId=incrementId();
+        this.text=text;
+        this.userId=userId;
+        this.postId=postId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+    public long getPostId() {
+        return postId;
+    }
+    public String getText() {
+        return text;
+    }
+    public long getCommentId() {
+        return commentId;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
