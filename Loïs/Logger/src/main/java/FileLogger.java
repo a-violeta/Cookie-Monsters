4f74750f2503a1937d294017f0b5c4f1c5a48847
Loïs CHAVIN <loischavin13@gmail.com>
2026-07-09 13:33:38 +0300

    Debug FileLogger not creating the file

--- Loïs/Logger/src/main/java/FileLogger.java
+++ Loïs/Logger/src/main/java/FileLogger.java
@@ -1,12 +1,17 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class FileLogger implements Logger {
+
     private String prefix;
+
     private static final String FILE_PATH = "log.txt";
-    LocalDate logDate = LocalDate.now();
+
+    //Create a Date Format excluding nanoseconds
+    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     public FileLogger(String prefix) {
         this.prefix = prefix;
@@ -14,10 +19,17 @@ public class FileLogger implements Logger {
 
     @Override
     public void log(String message) {
-        String formattedMessage = logDate + " / " + this.prefix + " / " + message;
+
+        //Get Local Date and Time
+        LocalDateTime logDate = LocalDateTime.now();
+
+        //Create a String of the Date with the created Format
+        String formattedMessage = logDate.format(dateFormat) + " / " + this.prefix + " / " + message;
+
         try (FileWriter fw = new FileWriter(FILE_PATH, true);
              PrintWriter pw = new PrintWriter(fw)) {
             pw.println(formattedMessage);
+            System.out.println(FILE_PATH);
         } catch (IOException e) {
             System.out.println("Error Impossible d'écrire dans " + FILE_PATH);
         }
--- Loïs/Logger/src/main/java/Main.java
+++ Loïs/Logger/src/main/java/Main.java
@@ -1,16 +1,13 @@
-// Main Class for usage of the Logger
 public class Main {
     public static void main() {
 
         // Configuration : tu choisis comment tu veux logger pour toute l'app
         Logger configBoth = new CompositeLogger(
-                new ConsoleLogger("CRITICAL"),
-                new FileLogger("WARNING")
+                new ConsoleLogger("DATE-TEST"),
+                new FileLogger("DATE-TEST")
         );
 
         LoggerFactory.configure(configBoth);
-
-        // N'importe où ailleurs dans ton code :
         Logger logger = LoggerFactory.getLogger();
         logger.log("Log Test");
     }
