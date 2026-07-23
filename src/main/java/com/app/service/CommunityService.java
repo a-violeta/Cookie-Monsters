4fcd151883563c6966120a19f9384c873dcd24fd
a-violeta <iulia.violeta.avram007@gmail.com>
2026-07-23 22:40:34 +0300

    Add @Transactional to methods and fix code
    I removed the removeUser() from Community model because it was barely used and it seemed better in the service class

--- src/main/java/com/app/service/CommunityService.java
+++ src/main/java/com/app/service/CommunityService.java
@@ -1,18 +1,15 @@
 package com.app.service;
 
 import com.app.model.Community;
-import com.app.model.Post;
 import com.app.model.User;
 import com.app.repository.CommunityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Optional;
-
+import java.util.Iterator;
 import java.util.List;
-
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +19,7 @@ public class CommunityService implements CommunityUseCases {
     //private final UserRepository userRepository;
     //private final PostRepository postRepository;
 
+    @Transactional(readOnly = true)
     public Community findCommunityById(long communityId) {
         return communityRepository.findById(communityId)
                 .orElseThrow(() ->
@@ -30,6 +28,7 @@ public class CommunityService implements CommunityUseCases {
                         ));
     }
 
+    @Transactional
     public void editCommunity(long communityId, String description) {
         Community community = findCommunityById(communityId);
         if (community != null) {
@@ -40,6 +39,7 @@ public class CommunityService implements CommunityUseCases {
         }
     }
 
+    @Transactional
     public void deleteCommunity(long communityId) {
 
         Community community = findCommunityById(communityId);
@@ -52,11 +52,13 @@ public class CommunityService implements CommunityUseCases {
         }
     }
 
+    @Transactional(readOnly = true)
     public List<Community> listCommunities() {
 
         return communityRepository.findAll();
     }
 
+    @Transactional
     public void joinCommunity(Long communityId, Long userId) {
         // right now, join means immediate approval into the community since we don't have admins or moderators yet
         Community community = findCommunityById(communityId);
@@ -67,10 +69,11 @@ public class CommunityService implements CommunityUseCases {
             throw new IllegalArgumentException("User is already part of the community");
         }
 
-        //community.addUser(user);
+        //community.getCommunityUsers().add(user);
         communityRepository.save(community);
     }
 
+    @Transactional
     public void exitCommunity(Long communityId, Long userId) {
         // exiting doesn't need approval
         // if the community has only one user then delete the community
@@ -83,36 +86,47 @@ public class CommunityService implements CommunityUseCases {
 
         if (community.getCommunityUsers().size() == 1) {
             throw new IllegalStateException("You are the last member. You cannot exit the community.");
-        } else {
-            // exit means removing person from community s communityUsers list
-            community.removeUser(userId);
-            communityRepository.save(community);
         }
-    }
 
-    public void removePostFromCommunity(Long communityId, Long postId) {
-        Community community = findCommunityById(communityId);
-        community.removePost(postId);
+        // exit means removing person from community s communityUsers list
+
+        Iterator<User> it = community.getCommunityUsers().iterator();
+        // removing from list by using iterator
+        while(it.hasNext()){
+            User u = it.next();
+            if(Objects.equals(u.getUserId(), userId)){
+                it.remove();
+                break;
+            }
+        }
+
         communityRepository.save(community);
     }
 
+    @Transactional
     public Community addCommunity(Community community) {
 
         if (communityRepository.existsByCommunityName(community.getCommunityName())) {
             throw new IllegalArgumentException("Community name is already taken");
         }
 
-        community.setId(null);
+        community.setCommunityId(null);
         return communityRepository.save(community);
     }
 
     // method necessary because in console you can't pass a Community as an argument
+    @Transactional
     public Community createCommunity(String communityName, String description){
 
         if (communityRepository.existsByCommunityName(communityName)) {
             throw new IllegalArgumentException("Community name is already taken");
         }
-        Community community = new Community(communityName, description, new ArrayList<>(), new ArrayList<>());
+        Community community = new Community();
+        community.setCommunityName(communityName);
+        community.setDescription(description);
+
+        // also, take the active user and add him to the community members
+
         return communityRepository.save(community);
     }
 }
