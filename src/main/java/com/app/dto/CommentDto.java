9c4e52f8f2522823cd81d0411131c54a1ec8852e
Loïs CHAVIN <loischavin13@gmail.com>
2026-08-10 16:38:35 +0300

    Imports Cleaning

--- src/main/java/com/app/dto/CommentDto.java
+++ src/main/java/com/app/dto/CommentDto.java
@@ -1,8 +1,6 @@
 package com.app.dto;
 
-import com.app.model.Comment;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
