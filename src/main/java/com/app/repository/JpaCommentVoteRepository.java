d125c5f86805f87d4ab343a15564effc2d7fde91
Andrei Preda <andreipreda998@gmail.com>
2026-08-10 22:52:51 +0300

    Add @Primary annotation for vote repositories

--- src/main/java/com/app/repository/JpaCommentVoteRepository.java
+++ src/main/java/com/app/repository/JpaCommentVoteRepository.java
@@ -1,9 +1,11 @@
 package com.app.repository;
 
 import com.app.model.CommentVote;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
+@Primary
 public interface JpaCommentVoteRepository extends JpaRepository<CommentVote, UUID>, CommentVoteRepository {
 }
--- src/main/java/com/app/repository/JpaPostVoteRepository.java
+++ src/main/java/com/app/repository/JpaPostVoteRepository.java
@@ -1,9 +1,11 @@
 package com.app.repository;
 
 import com.app.model.PostVote;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
+@Primary
 public interface JpaPostVoteRepository extends JpaRepository<PostVote, UUID>, PostVoteRepository {
 }
