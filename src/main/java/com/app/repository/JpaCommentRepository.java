efe47db6792e66e285b7c877115c6ff71014d076
CostinBMC <costin.badita.bmc@gmail.com>
2026-08-17 12:00:42 +0300

    Change the login behind the softdelete from manually using PostService.deletePost, CommentService.removeComment, UserService.deleteAccount one by one. Added dateOfBirth, getAge() and UserTooYpungException for maybe a new feature related to NSFW?
    Edit:Fixed the deleteposts problem V2

--- src/main/java/com/app/repository/JpaCommentRepository.java
+++ src/main/java/com/app/repository/JpaCommentRepository.java
@@ -1,7 +1,13 @@
 package com.app.repository;
 
 import com.app.model.Comment;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaCommentRepository extends JpaRepository<Comment, Long>, CommentRepository {
+import java.util.UUID;
+
+@Primary
+// fixed the ID type from Long to UUID to match the exact Comment entity primary key
+// (every other Jpa*Repository was already fixed this way - this one was missed)
+public interface JpaCommentRepository extends JpaRepository<Comment, UUID>, CommentRepository {
 }
